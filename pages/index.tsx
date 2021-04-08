afe4ef2e8d26d61da50223eacb6b88f1d0e325ee
shareef <nadeemshareef934@gmail.com>
2021-04-08 21:15:32 +0530

    Add _document.js and 404.js

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>YourNotes | Home</title>
+                <meta name="keywords" content="isl notes engineering shareef" />
+            </Head>
+            <div>
+                <h1>Hello</h1>
+            </div>
+        </>
+    );
+}

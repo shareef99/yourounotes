2b2c006306b4243c951aaa92392b9edcb8d6c086
shareef <nadeemshareef934@gmail.com>
2021-04-30 12:56:54 +0530

    add hotjar link

--- pages/_app.js
+++ pages/_app.js
@@ -44,6 +44,27 @@ function MyApp({ Component, pageProps }) {
                     href="/icons/favicon-16x16.png"
                 />
                 <link rel="manifest" href="/icons/site.webmanifest" />
+                {/* Hotjar Tracking Code for https://yourounotes.vercel.app/  */}
+                <script>
+                    {(function (h, o, t, j, a, r) {
+                        h.hj =
+                            h.hj ||
+                            function () {
+                                (h.hj.q = h.hj.q || []).push(arguments);
+                            };
+                        h._hjSettings = { hjid: 2370924, hjsv: 6 };
+                        a = o.getElementsByTagName("head")[0];
+                        r = o.createElement("script");
+                        r.async = 1;
+                        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(
+                        window,
+                        document,
+                        "https://static.hotjar.com/c/hotjar-",
+                        ".js?sv="
+                    )}
+                </script>
             </Head>
             <Layout head>
                 <Component {...pageProps} />

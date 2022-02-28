590f9b4effc5ba4d28a3eeab227cfbfdb72abd26
shareef <nadeemshareef934@gmail.com>
2022-02-28 10:16:06 +0530

    ✨ Update the tailwind version to V3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,75 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    mode: "jit",
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                bg: {
-                    DEFAULT: "#fffffe",
-                    dark: "#16161a",
-                },
-                heading: {
-                    // sub-heading, card-heading(dark also), card-paragraph = light mode
-                    DEFAULT: "#2b2c34",
-                    dark: "#fffffe",
-                },
-                para: {
-                    DEFAULT: "#2b2c34",
-                    dark: "#94a1b2", // sub-heading, card-paragraph = dark mode
-                },
-                btn: {
-                    DEFAULT: "#6246ea",
-                    dark: "#7f5af0",
-                },
-                btnText: {
-                    DEFAULT: "#fffffe",
-                    dark: "#fffffe",
-                },
-                cardBg: {
-                    DEFAULT: "#d1d1e9",
-                    dark: "#16161a",
-                },
-                error: {
-                    DEFAULT: "#ff0033",
-                },
-            },
-            spacing: {
-                "9/10": "90%",
-                "1/10": "10%",
-                "5%": "5%",
-                "7.5%": "7.5%",
-            },
-            maxWidth: {
-                "9/10": "90%",
-            },
-            transitionProperty: {
-                display: "display",
-            },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: {
+          DEFAULT: "#fffffe",
+          dark: "#16161a",
         },
-        screens: {
-            xs: "475px",
-            ...defaultTheme.screens,
+        heading: {
+          // sub-heading, card-heading(dark also), card-paragraph = light mode
+          DEFAULT: "#2b2c34",
+          dark: "#fffffe",
         },
-        container: {
-            center: true,
-            padding: "2rem",
+        para: {
+          DEFAULT: "#2b2c34",
+          dark: "#94a1b2", // sub-heading, card-paragraph = dark mode
         },
-        backgroundPosition: {
-            bottom: "bottom",
-            "bottom-4": "center bottom 4rem",
-            center: "center",
-            left: "left",
-            "left-bottom": "left bottom",
-            "left-top": "left top",
-            right: "right",
-            "right-bottom": "right bottom",
-            "right-top": "right top",
-            top: "top",
-            "top-4": "center top 1rem",
+        btn: {
+          DEFAULT: "#6246ea",
+          dark: "#7f5af0",
         },
+        btnText: {
+          DEFAULT: "#fffffe",
+          dark: "#fffffe",
+        },
+        cardBg: {
+          DEFAULT: "#d1d1e9",
+          dark: "#16161a",
+        },
+        error: {
+          DEFAULT: "#ff0033",
+        },
+      },
+      spacing: {
+        "9/10": "90%",
+        "1/10": "10%",
+        "5%": "5%",
+        "7.5%": "7.5%",
+      },
+      maxWidth: {
+        "9/10": "90%",
+      },
+      transitionProperty: {
+        display: "display",
+      },
     },
-    variants: {
-        extend: {},
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
     },
-    plugins: [],
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    backgroundPosition: {
+      bottom: "bottom",
+      "bottom-4": "center bottom 4rem",
+      center: "center",
+      left: "left",
+      "left-bottom": "left bottom",
+      "left-top": "left top",
+      right: "right",
+      "right-bottom": "right bottom",
+      "right-top": "right top",
+      top: "top",
+      "top-4": "center top 1rem",
+    },
+  },
 };
